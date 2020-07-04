a08589b245b314bf04830b0e4d4b07a31a77c322
Ahstermux <67801715+Ahstermux@users.noreply.github.com>
2020-07-05 01:13:44 +0700

    Update Ahs 2.sh

--- Ahs 2.sh
+++ Ahs 2.sh
@@ -20,40 +20,74 @@ read nomor
 
 if [ $nomor = 1 ] || [ $nomor = 01 ]
 then
-clear
-echo " Install JADWAL SHIFT" | lolcat
-pkg install bash
-git clone https://github.com/Ahstermux/Daftar-shift
-cd Daftar
-sh Daftar.sh
+clear 
+figlet SHIFT KERJA MALAM DAN PAGI | lolcat
+echo " SHIFT MALAM" | lolcat
+echo " 01. A SETETENGAH 12" | lolcat
+echo " 02. MAMANGLUBIZ" | lolcat
+echo " DST" | lolcat
+echo " SHIFT PAGI" | lolcat
+echo " 01. JAM 11" | lolcat
+echo " 02. Cak ganteng" |lolcat
+echo " 03. Plat l" | lolocat
+echo " 04. Cak r" | lolcat
 fi
 
 if [ $nomor = 2 ] || [ $nomor = 02 ]
 then
 clear
-echo " Install YANG DAFTAR.. " | lolcat
-pkg install bash 
-git clone https://github.com/Ahstermux/Daftar
-cd Daftar Lama
-sh Daftar Lama.sh
+figlet PENDAFTARAN ANGGOTA BARU | lolcat
+echo " [01] ANGGOTA BARU" | lolcat
+read nomor
+echo " [02] ALAMAT" | lolcat
+read nomor
+echo " [03] SEKOLAH" | lolcat
+read nomor
+echo " [04] KELAS" | lolcat
+read nomor
+echo " [04] NO TELPON" | lolcat
+read nomor
 fi
 
 if [ $nomor = 3 ] || [ $nomor = 03 ]
 then
 clear
-echo " Install PERATURAN.. " | lolcat
-pkg install bash 
-git clone https://github.com/Ahstermux/Rulle-3
-cd RULES
-sh RULES.sh
+figlet "PERATURAN AHS | lolcat
+echo " JANGAN MELANGGAR PERATURAN INI" | lolcat
+echo " HUKUMAN BAGI ANG MELANGGAR =" | lolcat
+echo " Daftar peraturan :" | lolcat
+echo " 1. Dilarang mengeluarkan anggota team dari group" | lolcat
 fi
 
 if [ $nomor = 4 ] || [ $nomor = 04 ]
 then
 clear
-echo " Install JADWAL KAS TEAM.. " | lolcat
-pkg install bash 
-git clone https://github.com/Ahstermux/KAS
-cd Kas
-sh Kas.sh
+figlet KAS 2K | lolcat
+echo " JADWAL KAS" | lolcat
+echo " Hari : Minggu" | lolcat
+echo " Wajib Kas 2K" | lolcat
+echo " Jika Tidak Membayar/Nunggak selama 1 Minggu MEMBAYAR 2X LIPAT!!!!!" | lolcat
+echo " Bendahara M Nur Alif" | lolcat
+echo " Sekertaris M Nur Alif " | lolcat
+echo " Nama yang sudah bayar kas :" | lolcat
+echo " 1. " | lolcat
+read nomor
+echo " 2. " | lolcat
+read nomor
+echo " 3. " | lolcat
+read nomor
+echo " 4. " | lolcat
+read nomor
+echo " 5. " | lolcat
+read nomor
+echo " 6. " | lolcat
+read nomor
+echo " 7. " | lolcat
+read nomor
+echo " 8. " | lolcat
+read nomor
+echo " 9. " | lolcat
+read nomor
+echo " 10. " | lolcat
+read nomor
 fi
